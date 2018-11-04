6dd94afae0943badf1203657631a9cb5db2ea9de
Arjun Vasudevan <arjunsv@berkeley.edu>
2018-11-04 01:29:40 -0800

    add visualization and piece movement

--- game.py
+++ game.py
@@ -1,50 +1,134 @@
-from utils import *
-
-class Game:
-	board = [[""]*5 for i in range(5)]
+from utils import input_to_coords, stringify_board
 
 class Piece:
-	def __init__(self, direction, position):
-		self.position = position
-		pass
 
-	def is_valid_move(self, destination):
-		pass
+	def __init__(self, direction, position, board):
+		""" Initializes piece
+		"""
+		self.direction = direction
+		self.position = position
+		self.board = board
+
+		if self.direction == "lower":
+			self.icon = self.icon.lower()
+
+
+	def move(self, src, dest):
+		""" Move a piece from start to destination if valid move.
+		"""
+
+		# TODO: change to function get_piece ?
+		piece = self.board.grid[src[0]][src[1]]
+		if self.is_valid_move(src, dest):
+			self.board.grid[src[0]][src[1]] = ""
+			self.board.grid[dest[0]][dest[1]] = piece
+		else:
+			print("INVALID MOVE")
+
+	def is_valid_move(self, src, dst):
+		""" Determines
+		"""
+		return True
+
 
 class King(Piece):
-	offsets = [(-1, 1), (0, 1), 
-			   (1, 1), (1, 0), 
-			   (1, -1), (0, -1), 
-			   (-1, -1), (-1, 0)]
+
+	icon = "K"
+	moves = [(-1, 1), (0, 1), 
+		 (1, 1), (1, 0), 
+		 (1, -1), (0, -1), 
+		 (-1, -1), (-1, 0)]
+
 
 class Rook(Piece):
-	offsets = [(0, 1), (0, 2), 
-		   	   (1, 0), (1, 2), 
-		       (0, -1), (0, -2), 
-		       (-1, 0), (-2, 0)]
+
+	icon = "R"
+	moves = [(0, 1), (0, 2), 
+		   	 (1, 0), (1, 2), 
+		     (0, -1), (0, -2), 
+		     (-1, 0), (-2, 0)]
+
 
 class Bishop(Piece):
-	offsets = [(-1, 1), (-2, 2), 
-		       (1, 1), (2, 2), 
-		       (1, -1), (2, -2), 
-		       (-1, -1), (-2, 2)]
+
+	icon = "B"
+	moves = [(-1, 1), (-2, 2), 
+		     (1, 1), (2, 2), 
+		     (1, -1), (2, -2), 
+		     (-1, -1), (-2, 2)]
+
 
 class GoldGeneral(Piece):
-	offsets = [(-1, 1), (0, 1), 
-		       (1, 1), (1, 0), 
-		       (0, -1), (-1, 0)]
+
+	icon = "G"
+	moves = [(-1, 1), (0, 1), 
+		     (1, 1), (1, 0), 
+		     (0, -1), (-1, 0)]
+
 
 class SilverGeneral(Piece):
-	offsets = [(-1, 1), (0, 1), 
-		       (1, 1), (1, -1), 
-		       (-1, -1)]
+
+	icon = "S"
+	moves = [(-1, 1), (0, 1), 
+		     (1, 1), (1, -1), 
+		     (-1, -1)]
 
 class Pawn(Piece):
-	offsets = [(0, 1)]
 
-class Player:
-	pass	
+	icon = "P"
+	moves = [(0, 1)]
+
+class Board:
+
+	def __init__(self):
+		""" Initializes board.
+		"""
+		self.grid = [[""]*5 for i in range(5)]
+		self.players = []
+		self.populate_grid()
+
+	def place_piece(self, piece, coords):
+		""" Places Piece at position coords on this Board's grid
+		"""
+		self.grid[coords[0]][coords[1]] = piece
+
+	def get_piece(self, coords):
+		""" Returns Piece if it exists at these coords
+		"""
+		return self.grid[coords[0]][coords[1]]
+
+	def remove_piece(self, coords):
+		""" Removes piece from grid if it exists at these coords
+		"""
+		self.grid[coords[0]][coords[1]] = ""
+
+	def populate_grid(self):
+		""" Populates pieces of initial board state.
+		"""
+		pieces = [King, GoldGeneral, SilverGeneral, Bishop, Rook]
+
+		# index into row col
+		for i in range(5):
+			# TODO: Abstract piece placement to function
+			self.place_piece(pieces[i]("UPPER", (4 - i, 4), self), (4 - i, 4))
+			self.place_piece(pieces[i]("lower", (i, 0), self), (i, 0))
+
+		# place pawns
+		self.place_piece(Pawn("UPPER", (0, 3), self), (0, 3))
+		self.place_piece(Pawn("lower", (4, 1), self), (4, 1))
 
 if __name__ == "__main__":
-	game = Game()
-	p1, p2 = Player(), Player()
+	board = Board()
+	game_over = False
+	is_lower_turn = True
+	while not game_over:
+		print(stringify_board(board.grid))
+		if is_lower_turn:
+			user_input = input("lower> ")
+		else:
+			user_input = input("UPPER> ")
+
+		src, dest = input_to_coords(user_input)
+		piece = board.get_piece(src)
+		piece.move(src, dest)
+		# make move here
--- utils.py
+++ utils.py
@@ -1,8 +1,24 @@
 import os
 
-def _stringifySquare(sq):
+def input_to_coords(user_input):
+    """ Convert string input into src and dest coordinate pair
+        Returns a list of two coordinate tuples
+    """
+    split_input = user_input.split(" ")
+    # command = split_input[0]
+    pos_1, pos_2 = split_input[1], split_input[2]
+    return (pos_to_cord(pos_1), pos_to_cord(pos_2))
+
+
+def pos_to_cord(pos_1):
+    """ Convert locs to pair of coordinate tuples
+    """
+    return (ord(pos_1[0]) - ord('a'), int(pos_1[1]) - 1)
+
+
+def _stringify_square(sq):
     if type(sq) is not str or len(sq) > 2:
-        raise ValueError('Board must be an array of strings like "", "P", or "+P"')
+        sq = sq.icon
 
     if len(sq) == 0:
         return '__|'
@@ -12,31 +28,31 @@ def _stringifySquare(sq):
         return sq + '|'
 
 
-def stringifyBoard(board):
+def stringify_board(board):
     s = ''
 
     for row in range(len(board) - 1, -1, -1):
         s += '' + str(row + 1) + ' |'
         for col in range(0, len(board[row])):
-            s += _stringifySquare(board[col][row])
+            s += _stringify_square(board[col][row])
         s += os.linesep
     s += '    a  b  c  d  e' + os.linesep
 
     return s
 
-def parseTestCase(path):
+def parse_test_case(path):
     f = open(path)
     line = f.readline()
-    initialBoardState = []
+    inital_board_state = []
     
     while line != '\n':
         piece, position = line.strip().split(' ')
-        initialBoardState.append(dict(piece=piece, position=position))
+        initial_board_state.append(dict(piece=piece, position=position))
         line = f.readline()
     line = f.readline().strip()
-    upperCaptures = [x for x in line[1:-1].split(' ') if x != '']
+    upper_captures = [x for x in line[1:-1].split(' ') if x != '']
     line = f.readline().strip()
-    lowerCaptures = [x for x in line[1:-1].split(' ') if x != '']
+    lower_captures = [x for x in line[1:-1].split(' ') if x != '']
     line = f.readline()
     line = f.readline()
     moves = []
@@ -44,4 +60,4 @@ def parseTestCase(path):
         moves.append(line.strip())
         line = f.readline()
 
-    return dict(initialPieces=initialBoardState, upperCaptures=upperCaptures, lowerCaptures=lowerCaptures, moves=moves)
+    return dict(initialPieces=inital_board_state, upperCaptures=upper_captures, lowerCaptures=lower_captures, moves=moves)
